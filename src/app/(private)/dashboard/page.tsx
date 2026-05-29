1241f5fa2845b2ffdcbed66fee9c3e194fbf48d3
shuhei_0720 <shuhei_0720@icloud.com>
2026-05-29 13:47:18 +0900

    記事一覧ページを作成

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -11,8 +11,37 @@ export default async function DashBoardPage() {
 
   const posts = await getOwnPosts(userId)
   return (
-    <div>
-      dashboard
+    <div className='p-4'>
+      <div className='flex justify-between'>
+        <h1 className='text-2xl font-bold mb-4'>記事一覧</h1>
+        <Button>新規記事作成</Button>
+      </div>
+      <table className='table-auto w-full border-collapse border'>
+        <thead>
+          <tr className='bg-gray-100'>
+            <th className='border p-2 text-center'>タイトル</th>
+            <th className='border p-2 text-center'>表示 / 非表示</th>
+            <th className='border p-2 text-center'>更新日時</th>
+            <th className='border p-2 text-center'>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <td className='border p-2'>{post.title}</td>
+              <td className='border p-2 text-center'>
+                {post.published ? '表示' : '非表示'}
+              </td>
+              <td className='border p-2 text-center'>
+                {new Date(post.updatedAt).toLocaleString()}
+              </td>
+              <td className='border p-2 text-center'>
+                <PostDropdownMenu postId={post.id} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
