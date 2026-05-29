66a4447d132848f9ee1315d16a249a068aada552
shuhei_0720 <shuhei_0720@icloud.com>
2026-05-29 13:37:06 +0900

    ログインユーザーの記事取得クエリを作成

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,4 +1,15 @@
+import { getOwnPosts } from '@/lib/ownPost'
+import { auth } from '@/auth'
+
+
 export default async function DashBoardPage() {
+  const session = await auth()
+  const userId = session?.user?.id
+  if (!session?.user?.email || !userId) {
+    throw new Error('不正なリクエストです')
+  }
+
+  const posts = await getOwnPosts(userId)
   return (
     <div>
       dashboard
